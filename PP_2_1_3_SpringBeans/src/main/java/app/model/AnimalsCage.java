3c511775bde2428fda8b9226c6dd7748d6f79e11
makeev <makeevstranik@gmail.com>
2022-06-11 04:15:10 +0500

    test passed

---
+++ PP_2_1_3_SpringBeans/src/main/java/app/model/AnimalsCage.java
@@ -0,0 +1,30 @@
+package app.model;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope("singleton")
+public class AnimalsCage {
+
+    @Autowired
+    @Qualifier("dog")
+    private Animal animal;
+
+    public Timer getTimer() {
+        return timer;
+    }
+
+    @Autowired
+    private Timer timer;
+
+    public void whatAnimalSay() {
+        System.out.println("Say:");
+        System.out.println(animal.toString());
+        System.out.println("At:");
+        System.out.println(timer.getTime());
+        System.out.println("________________________");
+    }
+}
